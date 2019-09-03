af17b02cde133d1c257f0ad16b74734f364367cf
Alexander <alex.sviatetskyi@gmail.com>
2019-09-03 18:47:31 +0300

    Create base project for Lab1.1

---
+++ DataSecurity.Lab1_1/IEncoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSecurity.Lab1_1
+{
+    public interface IEncoder
+    {
+        string EncoderName { get; set; }        
+        string Encrypt(string message);
+    }
+}
--- DataSecurity.Lab1_1/Program.cs
+++ DataSecurity.Lab1_1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DataSecurity.Lab1_1
 {
     class Program
     {
+        private static List<IEncoder> _encoders = new List<IEncoder>
+        {
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = Console.ReadLine();
+
+            foreach (var encoder in _encoders)
+            {
+                var encryptedMessage = encoder.Encrypt(message);
+                Console.WriteLine($"{encoder.EncoderName}: {encryptedMessage}");
+            }
         }
     }
 }
