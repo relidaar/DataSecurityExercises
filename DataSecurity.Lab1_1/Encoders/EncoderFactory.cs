7f38ef0a6f34ba3472da61014a0e1db055c399be
Alexander <alex.sviatetskyi@gmail.com>
2019-09-06 12:25:42 +0300

    Add factory for IEncoder

---
+++ DataSecurity.Lab1_1/Encoders/EncoderFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataSecurity.Lab1_1.Encoders.Implementations;
+using DataSecurity.Lab1_1.Encoders.Interfaces;
+
+namespace DataSecurity.Lab1_1.Encoders
+{
+    class EncoderFactory
+    {
+        public IEncoder UseCaesarCipher(int shift = 3) => new CaesarCipher(shift);
+        public IEncoder UseSloganCipher(string keyword = "secret") => new SloganCipher(keyword);
+        public IEncoder UsePolybiusSquare(string keyword = "") => new PolybiusSquare(keyword);
+        public IEncoder UseTrithemiusCipher(int a = 2, int b = 5, int c = 3) => new TrithemiusCipher(a, b, c);
+        public IEncoder UseVigenereCipher(string keyword = "secret") => new VigenereCipher(keyword);
+        public IEncoder UsePlayfairCipher(string keyword = "secret") => new PlayfairСipher(keyword);
+    }
+}
