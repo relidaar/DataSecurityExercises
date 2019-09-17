9f2032cc9b26f8310e0be7651e8e949fe0b5aa4e
Alexander <alex.sviatetskyi@gmail.com>
2019-09-17 17:30:16 +0300

    Add Lab1_4

---
+++ DataSecurity.Lab1_4/Encoder/BaseEncoder.cs
@@ -0,0 +1,7 @@
+﻿namespace DataSecurity.Lab1_4.Encoder
+{
+    abstract class BaseEncoder
+    {
+        public const string Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+}
---
+++ DataSecurity.Lab1_4/Encoder/IEncoder.cs
@@ -0,0 +1,9 @@
+﻿namespace DataSecurity.Lab1_4.Encoder
+{
+    public interface IEncoder
+    {
+        string Name { get; }
+        string Encrypt(string message);
+        string Decrypt(string encryptedMessage);
+    }
+}
