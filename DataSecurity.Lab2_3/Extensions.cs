a64bccce80c17066efbf28dc25d3b4a3fe40eba0
Alexander <alex.sviatetskyi@gmail.com>
2019-11-03 14:46:52 +0200

    Implement RSA digital signature

---
+++ DataSecurity.Lab2_3/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DataSecurity.Lab2_3
+{
+    static class Extensions
+    {
+        public static int GetNod(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b) a %= b;
+                else b %= a;
+            }
+
+            return Math.Max(a, b);
+        }
+
+        public static bool IsPrime(this int number)
+        {
+            if ((number & 1) == 0) return number == 2;
+
+            int limit = (int)Math.Sqrt(number);
+
+            for (int i = 3; i <= limit; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public static int GetPrime(int max, int min = 1)
+        {
+            var rnd = new Random();
+            int number = rnd.Next(min, max);
+            while (!number.IsPrime()) number = rnd.Next(min, max);
+
+            return number;
+        }
+    }
+}
---
+++ DataSecurity.Lab2_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DataSecurity.Lab2_3.Encoders;
+
+namespace DataSecurity.Lab2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter message: ");
+            var input = Console.ReadLine();
+
+            Console.WriteLine("\nRSA");
+            var rsaA = new RsaSender(150, 1000);
+            var rsaB = new RsaReceiver();
+
+            rsaA.GenerateKeys();
+            rsaA.CalculateH(input);
+            rsaA.CalculateS();
+            Console.WriteLine($"s = {rsaA.s}");
+
+            rsaB.CalculateH(input, rsaA.n);
+            var result = rsaB.Check(rsaA.s, rsaA.e, rsaA.n);
+
+            Console.WriteLine($"result: h' = h {result}");
+        }
+    }
+}
