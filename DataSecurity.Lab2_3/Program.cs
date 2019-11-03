e21a0351e22fcbf62669bfcf18154ba1fb2cacb7
Alexander <alex.sviatetskyi@gmail.com>
2019-11-03 17:51:40 +0200

    Implement GOST 34.10-94

--- DataSecurity.Lab2_3/Program.cs
+++ DataSecurity.Lab2_3/Program.cs
@@ -9,20 +9,49 @@ namespace DataSecurity.Lab2_3
         {
             Console.Write("Enter message: ");
             var input = Console.ReadLine();
+        }
 
+        static void Rsa(string input)
+        {
             Console.WriteLine("\nRSA");
-            var rsaA = new RsaSender(150, 1000);
-            var rsaB = new RsaReceiver();
+            var a = new RsaSender(150, 1000);
+            var b = new RsaReceiver();
 
-            rsaA.GenerateKeys();
-            rsaA.CalculateH(input);
-            rsaA.CalculateS();
-            Console.WriteLine($"s = {rsaA.s}");
+            a.GenerateKeys();
+            a.CalculateH(input);
+            a.CalculateS();
+            Console.WriteLine($"s = {a.s}");
 
-            rsaB.CalculateH(input, rsaA.n);
-            var result = rsaB.Check(rsaA.s, rsaA.e, rsaA.n);
+            b.CalculateH(input, a.n);
+            var result = b.Check(a.s, a.e, a.n);
 
             Console.WriteLine($"result: h' = h {result}");
+        }
+
+        static void Gost94(string input)
+        {
+            Console.WriteLine("\nGost94");
+            var a = new Gost94Sender(150, 1000);
+            var b = new Gost94Receiver();
+
+            Console.WriteLine("A: ");
+            a.GenerateKeys();
+            a.CalculateH(input);
+            while (a.s == 0 || a.w == 0)
+            {
+                a.CalculateW();
+                a.CalculateS();
+            }
+            Console.WriteLine($"w = {a.w}, s = {a.s}");
+
+            Console.WriteLine("\nB: ");
+            b.CalculateH(input);
+            b.CalculateV(a.q);
+            b.CalculateZ(a.s, a.w, a.q);
+            b.CalculateU(a.a, a.y, a.p, a.q);
+            var result = b.Check(a.w);
+
+            Console.WriteLine($"result: w' = u {result}");
         }
     }
 }
