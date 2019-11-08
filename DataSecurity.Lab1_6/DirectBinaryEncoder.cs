13cc452ca9e78a1667bd05379c692c5d47471024
Alexander <alex.sviatetskyi@gmail.com>
2019-11-08 09:20:21 +0200

    Add Lab1_6

---
+++ DataSecurity.Lab1_6/DirectBinaryEncoder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DataSecurity.Extensions;
+using DataSecurity.Interfaces;
+
+namespace DataSecurity.Lab1_6
+{
+    internal class DirectBinaryEncoder : IEncoder
+    {
+        public string Encode(string number)
+        {
+            if (string.IsNullOrEmpty(number)) throw new NullReferenceException();
+            if (number.Equals("0")) return "00000000";
+            if (number.Equals("-0")) return "10000000";
+            var num = Convert.ToInt32(number);
+
+            var result = Math.Abs(num).GetBinary().PadLeft(8, '0');
+
+            return num < 0 ? result.ReplaceAt(0, '1') : result;
+        }
+
+        public string Decode(string encryptedNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
