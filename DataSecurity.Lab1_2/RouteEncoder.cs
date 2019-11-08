2f128a65a0212d039b32494025308b24c2bec3d6
Alexander <alex.sviatetskyi@gmail.com>
2019-11-08 07:58:52 +0200

    Add Lab1_2

---
+++ DataSecurity.Lab1_2/RouteEncoder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using DataSecurity.Interfaces;
+
+namespace DataSecurity.Implementations.Lab1_2
+{
+    class RouteEncoder : IEncoder
+    {
+        private readonly int _numberOfColumns;
+        private const char Placeholder = '*';
+
+        public RouteEncoder(int numberOfColumns)
+        {
+            if (numberOfColumns <= 0) throw new Exception("Number of columns cannot be equal to or less than zero");
+
+            _numberOfColumns = numberOfColumns;
+        }
+
+        public string Encode(string message)
+        {
+            if (message == null) throw new NullReferenceException();
+
+            message = message.Replace(" ", "");
+            while (message.Length % _numberOfColumns != 0) message += Placeholder;
+
+            int n = message.Length / _numberOfColumns;
+            int m = _numberOfColumns;
+
+            var matrix = CreateMatrix(message, n, m);
+
+            var result = new StringBuilder();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result.Append(matrix[j, i]);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public string Decode(string encryptedMessage)
+        {
+            if (encryptedMessage == null) throw new NullReferenceException();
+
+            int n = encryptedMessage.Length / _numberOfColumns;
+            int m = _numberOfColumns;
+
+            var matrix = CreateMatrix(encryptedMessage, n, m, true);
+
+            var result = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    result.Append(matrix[i, j]);
+                }
+            }
+
+            return result.ToString().Trim(Placeholder);
+        }
+
+        private static char[,] CreateMatrix(string input, int n, int m, bool transposed = false)
+        {
+            var matrix = new char[n, m];
+
+            int index = 0;
+            if (transposed)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[j, i] = input[index];
+                        index++;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        matrix[i, j] = input[index];
+                        index++;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ DataSecurity.Lab1_2/SimpleTranspositionEncoder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataSecurity.Interfaces;
+
+namespace DataSecurity.Implementations.Lab1_2
+{
+    class SimpleTranspositionEncoder : IEncoder
+    {
+        private int[] _key;
+
+        public string Encode(string message)
+        {
+            if (message == null) throw new NullReferenceException();
+
+            message = message.Replace(" ", "");
+
+            _key = CreateKey(message.Length).ToArray();
+
+            var result = new StringBuilder();
+            foreach (var i in _key) result.Append(message[i]);
+
+            return result.ToString();
+        }
+
+        public string Decode(string encryptedMessage)
+        {
+            if (string.IsNullOrEmpty(encryptedMessage)) return null;
+
+            var result = new StringBuilder();
+            for (int i = 0; i < encryptedMessage.Length; i++)
+            {
+                var index = Array.IndexOf(_key, i);
+                result.Append(encryptedMessage[index]);
+            }
+
+            return result.ToString();
+        }
+
+        private static IEnumerable<int> CreateKey(int n)
+        {
+            var used = new List<int>();
+
+            var rnd = new Random();
+
+            // Generate random positions
+            for (int i = 0; i < n; i++)
+            {
+                int pos = rnd.Next(0, n);
+                while (used.Contains(pos)) pos = rnd.Next(0, n);
+
+                yield return pos;
+                used.Add(pos);
+            }
+        }
+    }
+}
