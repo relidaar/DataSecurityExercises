390fe87ed457e888c18ec1e1129c0ae6ec4096a1
Alexander <alex.sviatetskyi@gmail.com>
2019-11-08 07:30:33 +0200

    Add extensions

---
+++ DataSecurity.Extensions/EncoderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataSecurity.Extensions
+{
+    public static class EncoderExtensions
+    {
+        public static IEnumerable<char> GenerateAlphabet(int n, int shift)
+        {
+            for (int i = 0; i < n; i++) yield return (char)(i + shift);
+        }
+
+        public static string ReplaceAt(this string value, int index, char newChar) => 
+            value.Length <= index ? value : string.Concat(value.Select((c, i) => i == index ? newChar : c));
+
+        public static string GetBinary(this int value)
+        {
+            if (value == 0) return "";
+
+            int remainder = value % 2;
+            value /= 2;
+
+            return GetBinary(value) + remainder;
+        }
+
+        public static string GetFractionalBinary(this double value)
+        {
+            if (value == 0) return "0";
+
+            var fractional = new StringBuilder();
+            while (fractional.Length < 8)
+            {
+                value *= 2;
+                fractional.Append((int)value);
+                value = value < 1 ? value : value - (int)value;
+            }
+
+            return fractional.ToString();
+        }
+    }
+}
---
+++ DataSecurity.Extensions/MathExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace DataSecurity.Extensions
+{
+    public static class MathExtensions
+    {
+        public static int Mod(this int x, int m) => (x % m + m) % m;
+        public static double Mod(this double x, int m) => (x % m + m) % m;
+        public static BigInteger Mod(this BigInteger x, int m) => (x % m + m) % m;
+
+        public static int GetPrime(int max, int min = 1)
+        {
+            var rnd = new Random();
+            var value = rnd.Next(min, max);
+            while (!value.IsPrime()) value = rnd.Next(min, max);
+
+            return value;
+        }
+
+        public static bool IsPrime(this int value)
+        {
+            if (value % 1 == 0) return value == 2;
+
+            for (var i = 3; i <= Math.Sqrt(value); i += 2)
+            {
+                if (value % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public static int GetNod(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b) a %= b;
+                else b %= a;
+            }
+
+            return Math.Max(a, b);
+        }
+
+        public static int GetInverse(this int value, int n)
+        {
+            int inverse = 0;
+            while (inverse * value % n != 1) inverse++;
+
+            return inverse;
+        }
+    }
+}
